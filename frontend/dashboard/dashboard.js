5bf584486f3d03b55eead4427c91431665cd84a8
Utsav Sampat <usampat@uci.edu>
2024-05-13 14:42:26 -0700

    migrating to react

--- frontend/dashboard/dashboard.js
+++
@@ -1,6 +0,0 @@
-const togglebtn = document.querySelector('.togglebtn');
-const leftSide = document.querySelector('.left-side');
-
-togglebtn.addEventListener('click', () => {
-    leftSide.classList.toggle('collapsed');
-});
