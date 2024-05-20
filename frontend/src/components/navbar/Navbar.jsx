0806540232476a07abfd52a9212f19e276a55c6b
Utsav Sampat <usampat@uci.edu>
2024-05-20 09:00:27 -0700

    removed dashboard from navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,47 +1,42 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-import './Navbar.css'
+import "./Navbar.css";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleNavbar = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <nav className='navbar'>
-      <div className='navbar-brand'>AI LMS</div>
-      <div className='burger-menu' onClick={toggleNavbar}>
-        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+    <nav className="navbar">
+      <div className="navbar-brand">AI LMS</div>
+      <div className="burger-menu" onClick={toggleNavbar}>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
       </div>
-      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+      <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
         <li>
-          <Link to='/' onClick={toggleNavbar}>
+          <Link to="/" onClick={toggleNavbar}>
             Home
           </Link>
         </li>
         <li>
-          <Link to='/dashboard' onClick={toggleNavbar}>
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link to='/pricing' onClick={toggleNavbar}>
+          <Link to="/pricing" onClick={toggleNavbar}>
             Pricing
           </Link>
         </li>
         <li>
-          <Link to='/auth' onClick={toggleNavbar}>
+          <Link to="/auth" onClick={toggleNavbar}>
             Login/Signup
           </Link>
         </li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
